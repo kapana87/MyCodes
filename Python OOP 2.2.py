62d94c88af33e8ec48bde62737126d8da8d34d92
kapana87 <kapanadze87@gmail.com>
2023-07-19 00:12:21 +0400

    Implement the StrExtension class, which describes a set of functions for working with strings. When instantiated, the class must not take any arguments.
    The StrExtension class must have three static methods:
    
    remove_vowels() - a method that takes a string as an argument, removes all Latin vowels from it without regard to case and returns the result
    leave_alpha() - a method that takes a string as an argument, removes all non-Latin characters from it, and returns the result
    replace_all() is a method that takes three string arguments string, chars and char, replaces all characters in string string from chars to char case sensitively and returns the result.
    Note 1. It is guaranteed that all alphabetic characters belong to the Latin alphabet.
    
    Note 2. Latin vowels: a, e, i, o, u, y.
    
    Note 3. Additional data validation is not required. It is guaranteed that the implemented class is only used with valid data.

---
+++ Python OOP 2.2.py
@@ -0,0 +1,29 @@
+from io import StringIO
+
+
+class StrExtension:
+    @staticmethod
+    def remove_vowels(string):
+        vowels = ['a', 'e', 'i', 'o', 'u', 'y', 'A', 'E', 'I', 'O', 'U', 'Y']
+        string1 = ''
+        for i in string:
+            if i not in vowels:
+                string1 += i
+            else:
+                string1 += ''
+        return string1
+
+    @staticmethod
+    def leave_alpha(string):
+        values = StringIO()
+        for value in [char for char in string if char.isalpha()]:
+            values.write('' + value)
+        return values.getvalue()
+
+    @staticmethod
+    def replace_all(string, chars, char):
+        for i in chars:
+            string = string.replace(i, char)
+        return string
+
+
