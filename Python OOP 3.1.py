cc11d531823a25e1586eb8068283df566541fb35
kapana87 <kapanadze87@gmail.com>
2023-07-23 18:35:28 +0400

    Реализуйте класс Vector, описывающий вектор на плоскости. При создании экземпляра класс должен принимать два аргумента в следующем порядке:
    x — координата вектора по оси
    �
    x
    y — координата вектора по оси
    �
    y
    Экземпляр класса Vector должен иметь следующее формальное строковое представление:
    
    Vector(<координата x>, <координата y>)
    И следующее неформальное строковое представление:
    
    Вектор на плоскости с координатами (<координата x>, <координата y>)

---
+++ Python OOP 3.1.py
@@ -0,0 +1,16 @@
+class Vector:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def __str__(self):
+        return f"Вектор на плоскости с координатами ({self.x}, {self.y})"
+
+    def __repr__(self):
+        return f"{self.__class__.__name__}({self.x}, {self.y})"
+
+
+vector = Vector(1, 2)
+
+print(str(vector))
+print(repr(vector))
