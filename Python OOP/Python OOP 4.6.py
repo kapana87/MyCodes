d764899f9698f6952731d0771599331655c5339b
kapana87 <kapanadze87@gmail.com>
2023-07-30 22:00:14 +0400

    Реализуйте класс SuperString, описывающий строку. При создании экземпляра класс должен принимать один аргумент:
    string — значение строки
    Экземпляр класса SuperString должен иметь следующее неформальное строковое представление:
    
    <значение строки>
    Помимо этого, экземпляры класса SuperString должны поддерживать между собой операцию сложения с помощью оператора +, результатом которой должен являться новый экземпляр класса SuperString, представляющий конкатенацию исходных.
    
    Также экземпляр класса SuperString должен поддерживать операции умножения, деления, побитового сдвига влево и побитового сдвига вправо на целое число n с помощью операторов *, /, << и >> соответственно:
    
    результатом умножения должен являться новый экземпляр класса SuperString, представляющий исходную строку, умноженную на n
    результатом деления должен являться новый экземпляр класса SuperString, представляющий строку из первых m символов исходной строки, где m — длина исходной строки, поделенная нацело на n
    результатом побитового сдвига влево должен являться новый экземпляр класса SuperString, представляющий исходную строку без последних n символов. Если n больше или равно длине исходной строки, результатом должен являться экземпляр класса SuperString, представляющий пустую строку
    результатом побитового сдвига вправо должен являться новый экземпляр класса SuperString, представляющий исходную строку без первых n символов. Если n больше или равно длине исходной строки, результатом должен являться экземпляр класса SuperString, представляющий пустую строку
    Операция умножения должна быть выполнима независимо от порядка операндов, то есть должна быть возможность умножить как строку на число, так и число на строку.
    
    Примечание 1. Будем гарантировать, что экземпляр класса SuperString всегда делится на ненулевое число.
    
    Примечание 2. Если объект, с которым выполняется арифметическая операция, некорректен, метод, реализующий эту операцию, должен вернуть константу NotImplemented.

---
+++ Python OOP/Python OOP 4.6.py
@@ -0,0 +1,97 @@
+class PSuperString:
+    pass
+
+
+class SuperString(PSuperString):
+
+    def check_type(*arg_types):
+        def decorator(func):
+            def wrapped(self, other, *args, **kwargs):
+                if isinstance(other, arg_types):
+                    return func(self, other, *args, **kwargs)
+                return NotImplemented
+
+            return wrapped
+
+        return decorator
+
+    def __init__(self, string):
+        self.string = string
+
+    def __str__(self):
+        return f'{self.string}'
+
+    @check_type(PSuperString)
+    def __add__(self, other):
+        return SuperString(self.string + other.string)
+
+    @check_type(int, float)
+    def __mul__(self, other):
+        return SuperString(self.string * other)
+
+    @check_type(int, float)
+    def __rmul__(self, other):
+        return self.__mul__(other)
+
+    @check_type(int, float)
+    def __truediv__(self, other):
+        return SuperString(self.string[:int(len(self.string) / other)])
+
+    @check_type(int)
+    def __lshift__(self, other):
+        if other >= len(self.string):
+            return SuperString("")
+        elif other == 0:
+            return SuperString(self.string)
+        return SuperString(self.string[:-other])
+
+    @check_type(int)
+    def __rshift__(self, other):
+        if other >= len(self.string):
+            return SuperString("")
+        elif other == 0:
+            return SuperString(self.string)
+        return SuperString(self.string[other:])
+
+
+s1 = SuperString('bee')
+s2 = SuperString('geek')
+
+print(s1 + s2)
+print(s2 + s1)
+
+print()
+
+s = SuperString('beegeek')
+
+print(s * 2)
+print(3 * s)
+print(s / 3)
+
+print()
+
+s = SuperString('beegeek')
+
+print(s << 4)
+print(s >> 3)
+
+print()
+
+s = SuperString('beegeek')
+for i in range(9):
+    print(f'{s} << {i} =', s << i)
+
+print()
+
+superstring = SuperString('bee')
+print(superstring.__add__([]))
+print(superstring.__mul__(()))
+print(superstring.__truediv__({}))
+print(superstring.__lshift__(set()))
+print(superstring.__rshift__('geek'))
+
+print()
+
+s = SuperString('beegeek')
+for i in range(9):
+    print(f'{s} >> {i} =', s >> i)
