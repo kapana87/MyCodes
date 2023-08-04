94df83e62f4707f8a3100bff0e7ce289a8690190
kapana87 <kapanadze87@gmail.com>
2023-08-04 12:59:38 +0400

    Реализуйте декоратор @CachedFunction, который кэширует вычисленные значения декорируемой функции. Кэш должен быть доступен по атрибуту cache и представлять собой словарь, ключом в котором является кортеж с аргументами, а значением — возвращаемое значение декорируемой функции при вызове с этими аргументами.
    Примечание 1. Для однозначного кеширования гарантируется, что декорируемая функция принимает только позиционные аргументы.

---
+++ Python OOP/Callable objects/Decorator @CachedFunction.py
@@ -0,0 +1,38 @@
+class CachedFunction:
+    def __init__(self, func):
+        self.func = func
+        self.cache = {}
+
+    def __call__(self, *args):
+        if args not in self.cache:
+            self.cache[args] = self.func(*args)
+        return self.cache[args]
+
+
+@CachedFunction
+def slow_fibonacci(n):
+    if n == 1:
+        return 0
+    elif n in (2, 3):
+        return 1
+    return slow_fibonacci(n - 1) + slow_fibonacci(n - 2)
+
+
+print(slow_fibonacci(100))
+
+print()
+
+
+@CachedFunction
+def slow_fibonacci(n):
+    if n == 1:
+        return 0
+    elif n in (2, 3):
+        return 1
+    return slow_fibonacci(n - 1) + slow_fibonacci(n - 2)
+
+
+slow_fibonacci(5)
+
+for args, value in sorted(slow_fibonacci.cache.items()):
+    print(args, value)
