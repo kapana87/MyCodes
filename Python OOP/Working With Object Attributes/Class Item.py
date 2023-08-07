315ea210e37e1ffd059c5b183c480a3ea75de0cd
kapana87 <kapanadze87@gmail.com>
2023-08-07 20:57:24 +0400

    Требовалось реализовать класс Item, описывающий предмет. При создании экземпляра класс должен был принимать три аргумента в следующем порядке:
    name — название предмета
    price — цена предмета в рублях
    quantity — количество предметов
    Предполагалось, что при обращении к атрибуту name экземпляра класса Item будет возвращаться его название с заглавной буквы, а при обращении к атрибуту total — произведение цены предмета на его количество.
    
    Программист торопился и решил задачу неправильно. Дополните приведенный ниже код и реализуйте правильный класс Item.
    
    Примечание 1. Дополнительная проверка данных на корректность не требуется. Гарантируется, что реализованный класс используется только с корректными данными.

---
+++ Python OOP/Working With Object Attributes/Class Item.py
@@ -0,0 +1,21 @@
+class Item:
+    def __init__(self, name, price, quantity):
+        self.name = name
+        self.price = price
+        self.quantity = quantity
+
+    def __getattribute__(self, attr):
+        if attr == 'total':
+            self.total = self.price * self.quantity
+        elif attr == 'name':
+            return object.__getattribute__(self, attr).title()
+        return object.__getattribute__(self, attr)
+
+
+course = Item('pygen', 3900, 2)
+
+
+print(course.name)
+print(course.price)
+print(course.quantity)
+print(course.total)
