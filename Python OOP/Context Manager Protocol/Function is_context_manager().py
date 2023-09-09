822e3af1f9c8b5f4a7f970fbef676dfd95adf284
kapana87 <kapanadze87@gmail.com>
2023-09-10 00:26:33 +0400

    Реализуйте функцию is_context_manager(), которая принимает один аргумент:
    obj — произвольный объект
    Функция должна возвращать True, если объект obj является контекстным менеджером, или False в противном случае.
    
    Примечание 1. В тестирующую систему сдайте программу, содержащую только необходимую функцию is_context_manager(), но не код, вызывающий ее.

--- Python OOP/Context Manager Protocol/Function is_context_manager().py
+++ Python OOP/Context Manager Protocol/Function is_context_manager().py
@@ -1,18 +1,73 @@
-class CustomContextManager:
-    def __init__(self, value):
-        self.value = value
+def is_context_manager(obj):
+    if '__enter__' in obj.__dir__() and '__exit__' in obj.__dir__():
+        return True
+    return False
 
+
+# TEST_1:
+print(is_context_manager(open('output.txt', mode='w')))
+
+# TEST_2:
+from tempfile import TemporaryFile
+
+with TemporaryFile(mode='r+') as file:
+    print(is_context_manager(file))
+
+# TEST_3:
+from threading import Lock
+
+print(is_context_manager(Lock()))
+
+# TEST_4:
+print(is_context_manager(1992))
+print(is_context_manager('beegeek'))
+print(is_context_manager([1, 2, 3]))
+print(is_context_manager({'one': 1, 'two': 2}))
+print(is_context_manager(None))
+
+# TEST_5:
+class ContextManager:
     def __enter__(self):
-        print('Вход в контекстный менеджер...')
+        return 'beegeek'
+
+    def __exit__(self, exc_type, exc_value, exc_tb):
+        return True
+        
+
+print(is_context_manager(ContextManager()))
+
+# TEST_6:
+class ContextManager:
+    def __init__(self):
+        self.inside = False
+  
+    def __enter__(self):
+        self.inside = True
         return self
+  
+    def __exit__(self, exc_type, exc_value, exc_tb):
+        self.inside = False
+        return True
+        
+        
+print(is_context_manager(ContextManager()))
 
-    def __exit__(self, exc_type, exc_value, traceback):
-        print('Выход из контекстного менеджера...')
+# TEST_7:
+class ContextManager:
+    def __enter__(self):
+        return 'beegeek'
 
-    def __repr__(self):
-        return f'CustomContextManager(value={repr(self.value)})' 
 
+print(is_context_manager(ContextManager()))
 
-with CustomContextManager('pygen') as manager:
-    print(manager)
-    print(manager.value)
+# TEST_8:
+class ContextManager:
+    def __init__(self):
+        self.inside = False
+  
+    def __exit__(self, exc_type, exc_value, exc_tb):
+        self.inside = False
+        return True
+        
+        
+print(is_context_manager(ContextManager()))
